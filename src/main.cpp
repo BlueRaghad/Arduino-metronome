0088e46bb08220768310836c7edc890df72153b7
blueRaghad <rtrag1231234@gmail.com>
2026-03-06 07:05:19 +0300

    meeb

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@ pinMode(buzzPin, OUTPUT);
 }
 
 void loop() {
-  int potinValue = analogRead(potinPin);
-  int bpm = map(potinValue, 0, 1023, 40, 200);
-  int interval = 60000 / bpm;
+  int potinValue = analogRead(potinPin); // this is to read the potintiometer value 
+  int bpm = map(potinValue, 0, 1023, 40, 200); //here maping the the potintiometer values as a bpm
+  int interval = 60000 / bpm; //the time between each beat :) 
 
-  tone(buzzPin, 1000, 20);
+  tone(buzzPin, 1000, 20); //1000 is the frequency, 20 is the duration 
   delay(interval);
 }
