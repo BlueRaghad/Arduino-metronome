167efee1cf1975c56fcc837b780e01e9527c4039
blueRaghad <rtrag1231234@gmail.com>
2026-03-06 06:27:27 +0300

    the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 
-
+int potinPin = A0;
+int buzzPin = 8;
 
 void setup() {
-
+pinMode(buzzPin, OUTPUT);
 }
 
 void loop() {
+  int potinValue = analogRead(potinPin);
+  int bpm = map(potinValue, 0, 1023, 40, 200);
+  int interval = 60000 / bpm;
 
+  tone(buzzPin, 1000, 20);
+  delay(interval);
 }
-
-
-
